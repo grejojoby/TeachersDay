55a94794e7b1fbf5677196fc7e07376cf180c144
grejojoby <grejo00@gmail.com>
2024-09-05 16:54:18 +0530

    changes

--- script/main.js
+++ script/main.js
@@ -8,9 +8,9 @@ const animationTimeline = () => {
     .split("")
     .join("</span><span>")}</span`;
 
-  hbd.innerHTML = `<span>${hbd.innerHTML
+  hbd.innerHTML = `<span> ${hbd.innerHTML
     .split("")
-    .join("</span><span>")}</span`;
+    .join("</span><span> ")}</span`;
 
   const ideaTextTrans = {
     opacity: 0,
